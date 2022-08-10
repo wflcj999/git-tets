18c1ea573dc5346e1b62d751755daab0c0fc3a21
wmmcj <wmmcj99@qq.com>
2022-08-10 13:42:58 +0800

    IDEA second commit

--- src/main/java/com/wmm/git/GitTest.java
+++ src/main/java/com/wmm/git/GitTest.java
@@ -3,5 +3,6 @@ package com.wmm.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("IDEA  git !!!!");
+        System.out.println("IDEA2  git !!!!");
     }
 }
