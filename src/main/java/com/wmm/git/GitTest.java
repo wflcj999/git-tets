8ff15d7246ce9d7a175b3e3c4d3c511ce8bdfdb4
wmmcj <wmmcj99@qq.com>
2022-08-10 13:43:49 +0800

    IDEA third commit

--- src/main/java/com/wmm/git/GitTest.java
+++ src/main/java/com/wmm/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("IDEA  git !!!!");
         System.out.println("IDEA2  git !!!!");
+        System.out.println("IDEA3  git !!!!");
     }
 }
